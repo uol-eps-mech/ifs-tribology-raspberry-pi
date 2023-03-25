b4173e16e38d5aef2b8e744cee7e569ca25a6d17
Andy Blight <a.j.blight@leeds.ac.uk>
2023-03-25 20:56:34 +0000

    New logger produces the correct format output.

--- code/logger.py
+++ code/logger.py
@@ -1,21 +1,101 @@
 #!/usr/bin/env python3
-
+import os
 import time
 import random
+from datetime import datetime
 
-# Data generator.
+# The interval between each reading in milliseconds.
+INTERVAL_MS = 100
+DATA_FILENAME = "data.csv"
+# Output format. 1 for CSV else 0.
+OUTPUT_FORMAT_CSV = 1
 
-LIMIT_TIME = 100  # s
-DATA_FILENAME = "data.txt"
+
+class FakeMCC118DAQ:
+    """A fake version of the MCC-118 DAQ class that returns a value with some
+    random noise on it so that some changes are visible.
+    """
+
+    def __init__(self):
+        pass
+
+    def get(self, channel):
+        """Return a value simulating a voltage.
+        Each channel has a different central voltage.
+        """
+        if channel == 0:
+            value = 1.0 + random.random()
+        elif channel == 1:
+            value = 2.0 + random.random()
+        return value
 
 
-def gen_data(filename, limit_time):
-    start_time = time.time()
-    elapsed_time = time.time() - start_time
-    with open(filename, "w") as f:
-        while elapsed_time < limit_time:
-            f.write(f"{time.time():30.12f} {random.random():30.12f}\n")  # produces 64 bytes
-            f.flush()
-            elapsed_time = time.time() - start_time
+class Encoder:
+    def __init__(self):
+        pass
 
-gen_data(DATA_FILENAME, LIMIT_TIME)
+    def get(self):
+        # Fake value for now.
+        rpm = 25.0 + (random.random() * 0.5)
+        return rpm
+
+    def close(self):
+        pass
+
+
+class Logger:
+    def __init__(self):
+        self._encoder = Encoder()
+        self._daq = FakeMCC118DAQ()
+        self._running = True
+
+    def _voltage_to_friction(self, voltage):
+        # TODO some clever conversion.
+        return voltage
+
+    def _get_formatted_output(self):
+        # Get data.
+        voltage_0 = self._daq.get(0)
+        friction_0 = self._voltage_to_friction(voltage_0)
+        voltage_1 = self._daq.get(1)
+        friction_1 = self._voltage_to_friction(voltage_1)
+        rpm = self._encoder.get()
+        now = datetime.now()
+        now_string = now.strftime("%H:%M:%S.%f")
+        # Create formatted string.
+        if OUTPUT_FORMAT_CSV == 0:
+            output_string = "{}: RPM: {}, Angle 1: {}, Angle 2: {}\n".format(
+                now_string, rpm, friction_0, friction_1
+            )
+        else:
+            output_string = "{},{},{},{}\n".format(
+                now_string, rpm, friction_0, friction_1
+            )
+        # Debug. Can comment out if annoying.
+        print(output_string)
+        return output_string
+
+    def run(self, filename, interval_ms):
+        with open(filename, "w") as logger_file:
+            if OUTPUT_FORMAT_CSV == 1:
+                # Write header.
+                header_string = "Time,RPM,Angle 1,Angle 2\n"
+                print(header_string)
+                logger_file.write(header_string)
+            try:
+                while True:
+                    output_string = self._get_formatted_output()
+                    logger_file.write(output_string)
+                    time.sleep(interval_ms / 1000)
+            except KeyboardInterrupt:
+                print("Stopped by user")
+
+
+def main():
+    logger = Logger()
+    logger.run(DATA_FILENAME, INTERVAL_MS)
+    os.sync()
+
+
+if __name__ == "__main__":
+    main()
--- design.md
+++ design.md
@@ -6,10 +6,7 @@ The software for this project is formed of three main parts:
 2. Initial code started by Jack Rudd with a little polishing by me.
 3. The final version of code.
 
-All of the code can be found in the `code` directory.  The first versions of the code (in the `intern` directory ) were developed to get the system working and are a little rough around the edges.  They did the job but have the following problems:
-
-1. The process does not stop using `Ctrl C`.
-2. The graph program cannot be stopped easily by another thread.
+All of the code can be found in the `code` directory.  The first versions of the code (in the `intern` directory ) were developed to get the system working and are a little rough around the edges.  They did the job but had the problem that the data was written to disk in 4k blocks.  If the program was stopped before that, the data would be lost.  This caused by the abrupt exit of the program that prevented us from calling a function to flush the buffer to disk.
 
 After many hours of research and a lot of testing, I found [this answer on StackOverflow](https://stackoverflow.com/questions/72697369/real-time-data-plotting-from-a-high-throughput-source).  This does the classic separation of concerns thing by splitting the problem into two parts:
 
