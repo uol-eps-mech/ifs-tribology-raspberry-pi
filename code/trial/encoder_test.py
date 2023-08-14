4211b49e823dc43660c24726c883c0394b11ec31
jackrudd <mn20jkr@leeds.ac.uk>
2023-08-14 12:30:22 +0100

    encoder_test.py  Works using encoder class

--- code/trial/encoder_test.py
+++ code/trial/encoder_test.py
@@ -23,34 +23,33 @@ class Encoder:
     def __init__(self) -> None:
         self._rpm = 0.0
         self._last_datetime = datetime.now()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(ENCODER_Z_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(
+            ENCODER_Z_GPIO, GPIO.RISING, callback=self.interrupt_callback, bouncetime=200
+        )
 
     def get_rpm(self) -> float:
         return self._rpm
 
     def interrupt_callback(self, channel) -> None:
+        print("ISR called")
         # Each interrupt happens exactly once per revolution.
-        delta = datetime.now() - self._last_datetime
-        print(delta)
-        # Convert to RPM
+        now = datetime.now()
+        delta = now - self._last_datetime
+        # Convert to RPM.
         self._rpm = (1.0 / delta.total_seconds()) * 60
+        # Save now for later...
+        self._last_datetime = now
 
 
 def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
-def interrupt_callback(channel) -> None:
-    print("ISR called")
-
-
 def print_loop() -> None:
-    encoder = Encoder()
     print("Loop started")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(ENCODER_Z_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(
-        ENCODER_Z_GPIO, GPIO.RISING, callback=interrupt_callback, bouncetime=200
-    )
+    encoder = Encoder()
     while True:
         time.sleep(1.0)
         rpm = encoder.get_rpm()
